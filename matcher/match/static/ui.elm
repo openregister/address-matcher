1cac7ec8c4444c848eae987bf8b93ca45465cf46
Max Froumentin <max@froumentin.net>
2016-09-14 18:15:17 +0100

    Fetch randon addresses from the backend

--- matcher/match/static/ui.elm
+++ matcher/match/static/ui.elm
@@ -29,19 +29,51 @@ type alias User =
     }
 
 
+
+-- TODO: add test number from test dataset
+
+
+type alias TestAddress =
+    { address : String
+    }
+
+
+type alias TestAddresses =
+    List TestAddress
+
+
+type alias CandidateAddress =
+    { address : String
+    , uprn : String
+    }
+
+
 type alias Model =
-    { currentUserUri : String
+    { error : Maybe Error
+    , currentUserUri : String
     , users : List User
+    , testAddresses : TestAddresses
+    , candidateAddresses : List CandidateAddress
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { currentUserUri = "", users = [] }, fetchUsers )
+    (Model Nothing "" [] [] [])
+        ! [ fetchUsers, fetchTestAddresses ]
 
 
 
 -- HTTP
+
+
+jsonGet : String -> Request
+jsonGet url =
+    { verb = "GET"
+    , headers = [ ( "Accept", "application/json" ) ]
+    , url = url
+    , body = empty
+    }
 
 
 fetchUsers : Cmd Msg
@@ -49,15 +81,36 @@ fetchUsers =
     Task.perform
         FetchUsersFail
         FetchUsersOk
-        (Http.get (Json.Decode.list userDecoder) "/match/users/")
+        (fromJson usersDecoder (send defaultSettings (jsonGet "/match/users")))
 
 
-userDecoder : Decoder User
-userDecoder =
-    (Json.Decode.object2 User
-        ("name" := Json.Decode.string)
-        ("url" := Json.Decode.string)
+usersDecoder : Decoder (List User)
+usersDecoder =
+    (Json.Decode.list
+        (Json.Decode.object2 User
+            ("name" := Json.Decode.string)
+            ("url" := Json.Decode.string)
+        )
     )
+
+
+testAddressDecoder : Decoder (TestAddresses)
+testAddressDecoder =
+    (Json.Decode.list
+        (Json.Decode.object1 TestAddress
+            ("address" := Json.Decode.string)
+        )
+    )
+
+
+fetchTestAddresses : Cmd Msg
+fetchTestAddresses =
+    Task.perform
+        FetchTestAddressesFail
+        FetchTestAddressesOk
+        (fromJson testAddressDecoder
+            (send defaultSettings (jsonGet "/match/test-addresses/?n=5"))
+        )
 
 
 
@@ -69,6 +122,9 @@ type Msg
     | FetchUsersOk (List User)
     | FetchUsersFail Http.Error
     | UserChange String
+    | FetchTestAddresses
+    | FetchTestAddressesOk TestAddresses
+    | FetchTestAddressesFail Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -81,14 +137,33 @@ update msg model =
             ( { model | users = users }, Cmd.none )
 
         FetchUsersFail error ->
-            ( model, Cmd.none )
+            ( { model | error = Just error }, Cmd.none )
 
         UserChange userUri ->
             ( { model | currentUserUri = userUri }, Cmd.none )
 
+        FetchTestAddresses ->
+            ( model, fetchTestAddresses )
+
+        FetchTestAddressesOk addresses ->
+            ( { model | testAddresses = addresses }, Cmd.none )
+
+        FetchTestAddressesFail error ->
+            ( { model | error = Just error }, Cmd.none )
+
 
 
 -- VIEW
+
+
+testAddress : TestAddress -> Html Msg
+testAddress address =
+    li [] [ address.address |> text ]
+
+
+testAddresses : TestAddresses -> Html Msg
+testAddresses addresses =
+    ul [] (map testAddress addresses)
 
 
 userOption : User -> Html Msg
@@ -102,9 +177,16 @@ usersDropdown users =
         ((option [] [ text "Select a user" ]) :: (map userOption users))
 
 
+error : Maybe Error -> Html Msg
+error message =
+    div [] [ text (toString message) ]
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ usersDropdown model.users
-        , div [] [ text (toString model) ]
+        [ error model.error
+        , usersDropdown model.users
+        , testAddresses model.testAddresses
+          -- , div [] [ text (toString model) ]
         ]
