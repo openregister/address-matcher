c809c6b2b8bbd120ec6fc8c6eb29c12b83951ab9
Max Froumentin <max@froumentin.net>
2016-09-18 01:05:10 +0100

    Provide default for date

--- matcher/match/models.py
+++ matcher/match/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 class Address(models.Model):
     test_id = models.CharField(max_length=50, blank=True)
@@ -16,4 +17,4 @@ class Match(models.Model):
     test_address = models.ForeignKey(Address)
     user = models.ForeignKey(User)
     uprn = models.CharField(max_length=20)
-    date = models.DateField()
+    date = models.DateTimeField(default=datetime.now)
