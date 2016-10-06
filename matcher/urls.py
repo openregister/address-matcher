d0a2e46e22867de85128742e8a2a8b534b0ca88a
Max Froumentin <max@froumentin.net>
2016-10-06 17:06:14 +0100

    Redirect start page

--- matcher/urls.py
+++ matcher/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url="/match/main")),
     url(r'^admin/', admin.site.urls),
     url(r'^match/', include('match.urls'))
-
 ]
