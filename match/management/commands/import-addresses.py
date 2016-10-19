c260c9991fcacaa86696f8fc3aee4c1a80350d23
Max Froumentin <max@froumentin.net>
2016-10-19 12:32:55 +0100

    Make import commands read from stdin

--- match/management/commands/import-addresses.py
+++ match/management/commands/import-addresses.py
@@ -2,24 +2,20 @@ from django.core.management.base import BaseCommand, CommandError
 from match.models import Address
 
 import csv
+import sys
 
 class Command(BaseCommand):
-    help = 'Import a list of addresses from a file'
-
-    def add_arguments(self, parser):
-        # Positional arguments
-        parser.add_argument('filename')
+    help = 'Import a list of addresses stdin'
 
     def handle(self, *args, **options):
         # read a file and copy its contents as test addresses
 
         Address.objects.all().delete()
 
-        with open(options['filename'], 'r') as address_file:
-            tsvin = csv.reader(address_file, delimiter='\t')
+        tsvin = csv.reader(sys.stdin, delimiter='\t')
 
-            for row in tsvin:
-                Address.objects.create(
-                    test_id = row[0],
-                    address = ', '.join(row[1:])
-                )
+        for row in tsvin:
+            Address.objects.create(
+                test_id = row[0],
+                address = ', '.join(row[1:])
+            )
--- match/management/commands/import-users.py
+++ match/management/commands/import-users.py
@@ -2,21 +2,17 @@ from django.core.management.base import BaseCommand, CommandError
 from match.models import User
 
 import csv
+import sys
 
 class Command(BaseCommand):
-    help = 'Import a list of users from a file'
-
-    def add_arguments(self, parser):
-        # Positional arguments
-        parser.add_argument('filename')
+    help = 'Import a list of users from stdin'
 
     def handle(self, *args, **options):
         # read a file and copy its contents as test users
 
-        with open(options['filename'], 'r') as user_file:
-            tsvin = csv.reader(user_file, delimiter='\t')
+        tsvin = csv.reader(sys.stdin, delimiter='\t')
 
-            for row in tsvin:
-                User.objects.create(
-                    name = row[0]
-                )
+        for row in tsvin:
+            User.objects.create(
+                name = row[0]
+            )
