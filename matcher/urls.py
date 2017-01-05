32a0f4c402c795f621a76eca4f050d7521daf12a
Max Froumentin <max@froumentin.net>
2017-01-05 13:25:04 +0000

    Quick robots.txt

--- matcher/urls.py
+++ matcher/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url="/match/main")),
     url(r'^admin/', admin.site.urls),
-    url(r'^match/', include('match.urls'))
+    url(r'^match/', include('match.urls')),
+    url(
+        r'^robots.txt$',
+        lambda r: HttpResponse(
+            "User-agent: *\nDisallow: /",
+            content_type="text/plain")
+    )
 ]
